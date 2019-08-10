b69067652a13f6eb9e710e1576eb4eb026f7989a
actopozipc <48481041+actopozipc@users.noreply.github.com>
2019-08-10 15:55:19 +0200

    SQL-Methoden spezifiziert, Namen
    -Methoden und Klassen-Properties fangen jetzt alle gleich an
    -Eigene Methode für ,,grüne" Ausgabe (zwecks Leserlichkeit)
    -GenerateForEAchCountryAsync übernimmt jetzt zwei Parameter, die Länder und Kategorien einschränken
    -GetCountries kriegt jetzt Liste mit IDs
    -GenerateAsync kriegt jetzt Liste mit IDs von Kategorien
    -Kleiner Bug bei Country-Property von Countrystats-Klasse gefixt (war immer Austria, noch von Testzeiten)
    -GetCategoriesWithValues bekommt jetzt als Parameter die Liste mit KategorienIDs
    -Methoden mit SQL-Statements returnen nur noch Sachen wo die übergebenen IDs passen, zB GetCountries(13) return Austria weil Österreich die ID 13 hat in der DB
    -Methode ConcatSQLConditions zum Concatten von OR conditions in SQL-Statements geschrieben

--- ki/ki/DatenBerechnung.cs
+++ ki/ki/DatenBerechnung.cs
@@ -19,32 +19,32 @@ namespace ki
             connection = new NpgsqlConnection(cs);
             connection.Open();
         }
-        public async Task<List<Countrystats>> GenerateForEachCountryAsync()
+        public async Task<List<Countrystats>> GenerateForEachCountryAsync(List<int> laenderIDs, List<int> kategorienIDs)
         {
-            List<string> laender =  GetCountries(new List<int>()); //Liste mit allen Ländern
+            List<string> laender = GetCountries(laenderIDs); //Liste mit allen Ländern
             List<Countrystats> countrystats = new List<Countrystats>();
             for (int i = 0; i < laender.Count; i++)
             {
                 await Task.Run(async () =>
                  {
                      Countrystats c = new Countrystats();
-                     c.country = laender[i];
-                     c.ListWithCategoriesWithYearsAndValues = await GenerateAsync(laender[i]);
+                     c.Country = laender[i];
+                     c.ListWithCategoriesWithYearsAndValues = await GenerateAsync(laender[i], kategorienIDs);
                      countrystats.Add(c);
 
                  });
 
-                Console.WriteLine("Land {0} wird gelernt", laender[i]);  
+                Console.WriteLine("Land {0} wurde berechnet", laender[i]);
             }
             return countrystats;
         }
-        public async Task<List<CategoriesWithYearsAndValues>> GenerateAsync(string country)
+        public async Task<List<CategoriesWithYearsAndValues>> GenerateAsync(string country, List<int> kategorienIDs)
         {
-            
+
             Countrystats countrystats = new Countrystats(); //Klasse für alle Kategorien und deren Werte per Jahr
-            countrystats.country = "Austria"; //Land zu dem die Kategorien mit Werte gehören
+            countrystats.Country = country; //Land zu dem die Kategorien mit Werte gehören
             //in der Datenbank sind derzeit nur Werte für Österreich drin
-            countrystats.ListWithCategoriesWithYearsAndValues = await getCategoriesWithValuesAndYearsAsync(country); //Werte mit Jahren
+            countrystats.ListWithCategoriesWithYearsAndValues = await GetCategoriesWithValuesAndYearsAsync(country, kategorienIDs); //Werte mit Jahren
             categorycount = countrystats.ListWithCategoriesWithYearsAndValues.Count; //wie viele kategorien an daten für dieses land existieren
             List<CategoriesWithYearsAndValues> CategorysWithFutureValues = new List<CategoriesWithYearsAndValues>();
             Task<List<YearWithValue>>[] liste = new Task<List<YearWithValue>>[categorycount]; //liste damit jede kategorie in einem task abgearbeitet werden kann
@@ -55,30 +55,30 @@ namespace ki
 
                 //Erstelle für jede Kategorie einen Liste mit eigenen Datensätzen
                 List<YearWithValue> SingleCategoryData = new List<YearWithValue>();
-               
+
                 //Hole einzelne Datensätze für jedes Jahr heraus
                 foreach (var YearWithValue in countrystats.ListWithCategoriesWithYearsAndValues[i].YearsWithValues)
                 {
-                    
-                    SingleCategoryData.Add(new YearWithValue(YearWithValue.year, YearWithValue.value, countrystats.ListWithCategoriesWithYearsAndValues[i].category));
+
+                    SingleCategoryData.Add(new YearWithValue(YearWithValue.Year, YearWithValue.Value, countrystats.ListWithCategoriesWithYearsAndValues[i].category));
                 }
                 //Wenn ein Wert nicht dokumentiert ist, ist in der Datenbank 0 drin. Das verfälscht den Wert für die Ki
                 //entferne deswegen 0
-                SingleCategoryData = removeZero(SingleCategoryData);
+                SingleCategoryData = RemoveZero(SingleCategoryData);
                 //Wenn es mindestens ein Jahr einer Kategorie gibt, in der der Wert nicht 0 ist
                 if (SingleCategoryData.Count > 1)
                 {
 
                     //Bearbeite eigenen Datensatz
-                    int multi = scale(SingleCategoryData) -1; //wie viel man die normierten werte mulitplizieren muss damit sie wieder echt sind
+                    int multi = Scale(SingleCategoryData) - 1; //wie viel man die normierten werte mulitplizieren muss damit sie wieder echt sind
                     //Erstelle Task für einzelnen Datensatz um dann auf alle zu warten
-                    liste[i] =  Task<List<YearWithValue>>.Run(() =>
-                        {
-                            return Train(SingleCategoryData, 2030, multi);
-                        });
+                    liste[i] = Task<List<YearWithValue>>.Run(() =>
+                       {
+                           return Train(SingleCategoryData, 2030, multi);
+                       });
 
                 }
-                //ohne dieses else gebe es einige leere Tasks im Array -> Exception
+                //ohne dieses else gäbe es einige leere Tasks im Array -> Exception
                 //ohne if geht die KI datensätze ohne einträge durch -> Verschwendung von Rechenleistung und Zeit
                 else
                 {
@@ -111,22 +111,22 @@ namespace ki
             //Für jedes Jahr mit Wert in der Liste mit Jahren und Werten d
             foreach (var YearWithValue in KnownValues)
             {
-                inputs.Add(Convert.ToDouble(YearWithValue.year));
-                outputs.Add(Convert.ToDouble(YearWithValue.value) / (Math.Pow(10, multi)));
+                inputs.Add(Convert.ToDouble(YearWithValue.Year));
+                outputs.Add(Convert.ToDouble(YearWithValue.Value) / (Math.Pow(10, multi)));
             }
-             Input input =  standardization(inputs, FutureYear);
+            Input input = Standardization(inputs, FutureYear);
             Neuron hiddenNeuron1 = new Neuron();
             Neuron outputNeuron = new Neuron();
             hiddenNeuron1.randomizeWeights();
             outputNeuron.randomizeWeights();
 
-            
+
             int lernvorgang = 0;
             int z = KnownValues.Count;
             //Trainiere alle bekannten Werte x mal
             while (lernvorgang < x)
             {
-               
+
                 for (int i = 0; i < z; i++)
                 {
                     hiddenNeuron1.inputs = input.getNormierterWert(inputs[i]);
@@ -135,14 +135,14 @@ namespace ki
                     outputNeuron.adjustWeights();
                     hiddenNeuron1.error = sigmoid.derived(hiddenNeuron1.output) * outputNeuron.error * outputNeuron.weights;
                     hiddenNeuron1.adjustWeights();
-                   
+
                 }
-            
+
                 lernvorgang++;
             }
-          
+
             //bekomme immer das höchste jahr
-            double j = KnownValues.Max(i => i.year);
+            double j = KnownValues.Max(i => i.Year);
 
             //wenn das höchste bekannte jahr kleiner ist als das Jahr, bis zu dem wir die Werte wissen wollen, 
             //dann füge das nächste Jahr als input ins Neuron, bekomme den Output und füge es in die Liste mit allen Werten ein
@@ -152,7 +152,7 @@ namespace ki
 
                 hiddenNeuron1.inputs = input.getNormierterWert(j) + input.step;
                 outputNeuron.inputs = hiddenNeuron1.output;
-                KnownValues.Add(new YearWithValue((Math.Round((inputs[inputs.Count-1]+ 1))), Convert.ToDecimal(outputNeuron.output * Convert.ToDouble(Math.Pow(10,multi)))));
+                KnownValues.Add(new YearWithValue((Math.Round((inputs[inputs.Count - 1] + 1))), Convert.ToDecimal(outputNeuron.output * Convert.ToDouble(Math.Pow(10, multi)))));
                 return Train(KnownValues, FutureYear, multi);
             }
             //wenn alle Jahre bekannt sind, returne die Liste
@@ -162,31 +162,31 @@ namespace ki
             }
 
         }
-        Input standardization(List<double> inputs, int Zukunftsjahr)
+        Input Standardization(List<double> inputs, int Zukunftsjahr)
         {
             Input input = new Input();
-            inputs = inputs.Distinct().ToList(); //Ich weiß dass ich ein Hashset verwenden könnte, aber solange es nicht fix ist dass der bug von hier kommt reicht das
+            inputs = inputs.Distinct().ToList(); //Ich weiß dass ich ein Hashset verwenden könnte, aber ich weiß nicht ob sich das von der Performance lohnt. Add in Hashset = braucht länger als liste, dafür konsumiert liste.distinct zeit
 
             double maxvalue = inputs.Max();
             double count = inputs.Count;
             double diff = Zukunftsjahr - maxvalue;
-            double step = 1 / (count+diff);
+            double step = 1 / (count + diff);
             List<double> normierteWerte = new List<double>();
             input.step = step;
             double i = 0;
-           
-                foreach (var item in inputs)
-                {
+
+            foreach (var item in inputs)
+            {
                 input.Add(item, i);
                 i = i + step;
-                }
+            }
 
             return input;
         }
-        int scale(List<YearWithValue> n)
+        int Scale(List<YearWithValue> n)
         {
 
-            double temp = Convert.ToDouble(n.Max(i => i.value));
+            double temp = Convert.ToDouble(n.Max(i => i.Value));
             int m = 1;
             while (1 <= temp)
             {
@@ -196,24 +196,23 @@ namespace ki
 
             return m;
         }
-        List<YearWithValue> removeZero(List<YearWithValue> collection)
+        List<YearWithValue> RemoveZero(List<YearWithValue> collection)
         {
-            var temp = collection.Where(i => i.value != 0).ToList();
+            var temp = collection.Where(i => i.Value != 0).ToList();
             return temp;
         }
-        public async Task<List<CategoriesWithYearsAndValues>> getCategoriesWithValuesAndYearsAsync(string country)
+        public async Task<List<CategoriesWithYearsAndValues>> GetCategoriesWithValuesAndYearsAsync(string country, List<int> kategorieIDs)
         {
             NpgsqlCommand command = connection.CreateCommand();
-            List<string> vs = GetItems();
+            List<string> vs = GetItems(kategorieIDs);
             List<CategoriesWithYearsAndValues> keyValuePairs = new List<CategoriesWithYearsAndValues>();
             foreach (var item in vs)
             {
-                if (item != "Barbados" )
-                {
 
-               
-                CategoriesWithYearsAndValues kmjw = new CategoriesWithYearsAndValues();
-                kmjw.category = item;
+                CategoriesWithYearsAndValues kmjw = new CategoriesWithYearsAndValues
+                {
+                    category = item
+                };
                 command.CommandText = $"SELECT year, ROUND(values, 15) FROM input_data JOIN category c on input_data.cat_id = c.cat_id JOIN country_or_area coa on input_data.coa_id = coa.coa_id WHERE c.name = '{item}' AND coa.name = '{country}';";
                 using (NpgsqlDataReader reader = command.ExecuteReader())
                 {
@@ -228,15 +227,28 @@ namespace ki
                     kmjw.YearsWithValues = temp;
                     keyValuePairs.Add(kmjw);
                 }
-                }
+
             }
             return keyValuePairs;
 
         }
-        public List<string> GetItems()
+        public List<string> GetItems(List<int> catids)
         {
             NpgsqlCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT name FROM category ORDER BY category;";
+            if (catids.Count == 0)
+            {
+                command.CommandText = "SELECT name FROM category ORDER BY category;";
+            }
+            else
+            {
+                List<string> statementConditions = new List<string>();
+                foreach (var item in catids)
+                {
+                    statementConditions.Add("coa_id=" + item);
+                }
+                command.CommandText = ConcatSQLConditions(statementConditions, "SELECT name FROM country_or_area");
+            }
+
             List<string> disziplin = new List<string>();
             using (NpgsqlDataReader reader = command.ExecuteReader())
             {
@@ -250,29 +262,38 @@ namespace ki
         public List<string> GetCountries(List<int> coaids)
         {
             NpgsqlCommand command = connection.CreateCommand();
-            if(coaids.Count == 0)
+            if (coaids.Count == 0)
             {
-                  command.CommandText = "SELECT name FROM country_or_area;";
+                command.CommandText = "SELECT name FROM country_or_area;";
             }
             else
             {
-                string temp = "";
-                foreach(var item in coaids)
+                List<string> statementConditions = new List<string>();
+
+                foreach (var item in coaids)
                 {
-                    //todo
+                    statementConditions.Add("coa_id=" + item);
                 }
+                ConcatSQLConditions(statementConditions, "SELECT name FROM country_or_area");
+
+                command.CommandText = ConcatSQLConditions(statementConditions, "SELECT name FROM country_or_area");
+
             }
-          
+
             List<string> land = new List<string>();
             using (NpgsqlDataReader reader = command.ExecuteReader())
             {
-                while ( reader.Read()) //fraglich ob es nicht eine bessere Methode gibt
+                while (reader.Read()) //fraglich ob es nicht eine bessere Methode gibt
                 {
                     land.Add((string)reader["name"]);
                 }
             }
             return land;
         }
-      
+        private string ConcatSQLConditions(List<string> ids, string statement)
+        {
+            string temp = String.Join("OR", ids);
+            return statement + "WHERE " + temp + ";";
+        }
     }
 }
--- ki/ki/Program.cs
+++ ki/ki/Program.cs
@@ -17,37 +17,43 @@ namespace ki
             Console.WriteLine("Gestartet um " +DateTime.Now);
             Stopwatch sw = new Stopwatch();
             sw.Start();
-           var liste =  Task.Run(async () =>
+            var liste =  Task.Run(async () =>
             {
-               return await asynchroneMainMethodenWerdenErstAbCsharp7ImplementiertAsync();
+               return await AsynchroneMainMethodenWerdenErstAbCsharp7ImplementiertAsync();
             }).GetAwaiter().GetResult();
             sw.Stop();
             Console.WriteLine("Elapsed={0}", sw.Elapsed);
             Console.ReadKey();
             foreach (var LandMitKategorieUndAllenDatenJahren in liste)
             {
+                Printf("<======"+LandMitKategorieUndAllenDatenJahren.Country+"======>");
+
                 foreach (var KategorieMitJahrenUndWerten in LandMitKategorieUndAllenDatenJahren.ListWithCategoriesWithYearsAndValues)
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(KategorieMitJahrenUndWerten.category);
-                    Console.ForegroundColor = ConsoleColor.White;
+                   
+                    Printf(KategorieMitJahrenUndWerten.category);
+                
                     foreach (var JahreMitWerten in KategorieMitJahrenUndWerten.YearsWithValues)
                     {
-                        Console.WriteLine($"{JahreMitWerten.year} : {JahreMitWerten.value}");
+                        Console.WriteLine($"{JahreMitWerten.Year} : {JahreMitWerten.Value}");
                     }
                 }
             }
         }
-        static async Task<List<Countrystats>> asynchroneMainMethodenWerdenErstAbCsharp7ImplementiertAsync()
+        static async Task<List<Countrystats>> AsynchroneMainMethodenWerdenErstAbCsharp7ImplementiertAsync()
         {
           
             CalculateData calc = new CalculateData();
           
-             var liste = await calc.GenerateForEachCountryAsync();
+             var liste = await calc.GenerateForEachCountryAsync(new List<int>(), new List<int>());
             return liste;
-           
             
-            
+        }
+        static void Printf(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = ConsoleColor.White;
         }
        
     }
